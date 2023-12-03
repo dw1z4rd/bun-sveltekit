3c27e6834714b704a2ba9df31d711a9759ea1979
root <root@PanDEMONium.>
2023-12-02 20:09:14 -0500

    Removed redundant code in src/routes/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,38 @@
+<!-- @format -->
 <script lang="ts">
-    import { onDestroy } from 'svelte';
-    import { count, increment, decrement } from '../stores';
+  import { onDestroy } from "svelte";
+  import { count, decrement, increment } from "../stores";
 
-    let countVal: number;
+  let countVal: number;
 
-    const unsubscribe = count.subscribe(val => {
-        countVal = val;
-    })
+  const unsubscribe = count.subscribe((val) => {
+    countVal = val;
+  });
 
-    onDestroy(unsubscribe);
+  onDestroy(unsubscribe);
 
-    const handleIncrement = () => {
-        increment();
-    }
-
-    const handleDecrement = () => {
-        decrement();
-    }
-
-    $: isNeg = countVal < 0 ? true : false;
-    $: isZero = countVal === 0 ? true : false;
-    $: isPos = countVal > 0 ? true : false;
-
+  $: isNeg = countVal < 0 ? true : false;
+  $: isZero = countVal === 0 ? true : false;
+  $: isPos = countVal > 0 ? true : false;
 </script>
 
 <div class="w-1/2 mx-auto block p-6">
-    <h1 class:text-red-700={isNeg} class:text-slate-700={isZero} class:text-emerald-700={isPos} class="text-8xl font-bold text-center">{countVal}</h1>
+  <h1
+    class:text-red-700={isNeg}
+    class:text-slate-700={isZero}
+    class:text-emerald-700={isPos}
+    class="text-8xl font-bold text-center"
+  >
+    {countVal}
+  </h1>
 </div>
 <div class="w-full block p-8">
-    <button class="p-2 rounded-xl bg-emerald-700 text-white w-28 float-left" on:click={handleDecrement}>-</button>
-    <button class="p-2 rounded-xl bg-emerald-700 text-white w-28 float-right" on:click={handleIncrement}>+</button>
+  <button
+    class="p-2 rounded-xl bg-emerald-700 text-white w-28 float-left"
+    on:click={decrement}>-</button
+  >
+  <button
+    class="p-2 rounded-xl bg-emerald-700 text-white w-28 float-right"
+    on:click={increment}>+</button
+  >
 </div>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import type { UserConfig } from "vite";
 
 const config: UserConfig = {
   plugins: [sveltekit()],
+  server: {
+    port: 3000
+  },
+  preview: {
+    port: 3000
+  }
 };
 
 export default config;
